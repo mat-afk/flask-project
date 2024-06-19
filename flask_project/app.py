6b36316b59402e60198394df28568eb3aea55332
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-06-19 17:54:03 -0300

    feat: add cadastro produto endpoint

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from .domain.produtos import produtos as produtos_list
+from flask import Flask, render_template, redirect, url_for, request
+from .domain.produtos import produtos as produtos_list, Produto
 
 app = Flask(__name__)
 
@@ -15,17 +15,36 @@ def contato():
 
 @app.route("/produtos")
 def produtos():
-    return render_template("produtos.html", produtos=produtos_list)
+    return render_template("produtos/produtos.html", produtos=produtos_list)
 
 
 @app.route("/produtos/<name>")
 def produto(name):
     for produto in produtos_list:
-        if produto["slug"] == name:
-            return render_template("produto.html", produto=produto)
+        if produto.slug == name:
+            return render_template("produtos/produto.html", produto=produto)
         
     return "Erro: Produto não encontrado"
-            
+
+
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+    return render_template("produtos/cadastro_produto.html")
+
+
+@app.route("/produtos", methods=["POST"])
+def salvar_produto():
+    name = request.form["name"]
+    description = request.form["description"]
+    price = request.form["price"]
+    image_url = request.form["image_url"]
+
+    produto = Produto(name, description, price, image_url)
+
+    produtos_list.append(produto)
+
+    return redirect(url_for("produtos"))
+
 
 if __name__ == "__main__":
     app.run()
